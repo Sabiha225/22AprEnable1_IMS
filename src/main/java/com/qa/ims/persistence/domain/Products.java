720765d5363135792efa8d7969bab3f30922d969
Sabiha225 <sabiha_alam@hotmail.com>
2022-05-04 19:04:00 +0100

    update changes to feature_domain

--- src/main/java/com/qa/ims/persistence/domain/Products.java
+++ src/main/java/com/qa/ims/persistence/domain/Products.java
@@ -4,42 +4,42 @@ import java.util.Objects;
 
 public class Products {
 	
-	private Long id;
-	private String name;
+	private Long product_id;
+	private String product_name;
 	private Double cost;
 	
-	public Products(Long id, String name, Double cost) {
+	public Products(Long product_id, String product_name, Double cost) {
 		super();
-		this.id = id;
-		this.name = name;
+		this.product_id = product_id;
+		this.product_name = product_name;
 		this.cost = cost;
 	}
 	
   
 	public Products(String name, Double cost) {
 		super();
-		this.name = name;
+		this.product_name = name;
 		this.cost = cost;
 	}
 
 
 	public Long getId() {
-		return id;
+		return product_id;
 	}
 
 
-	public void setId(Long id) {
-		this.id = id;
+	public void setId(Long product_id) {
+		this.product_id = product_id;
 	}
 
 
 	public String getName() {
-		return name;
+		return product_name;
 	}
 
 
-	public void setName(String name) {
-		this.name = name;
+	public void setName(String product_name) {
+		this.product_name = product_name;
 	}
 
 
@@ -55,13 +55,13 @@ public class Products {
 
 	@Override
 	public String toString() {
-		return "Products [id=" + id + ", name=" + name + ", cost=" + cost + "]";
+		return "Products [id=" + product_id + ", name=" + product_name + ", cost=" + cost + "]";
 	}
 
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(cost, id, name);
+		return Objects.hash(cost, product_id, product_name);
 	}
 
 
@@ -74,7 +74,7 @@ public class Products {
 		if (getClass() != obj.getClass())
 			return false;
 		Products other = (Products) obj;
-		return Objects.equals(cost, other.cost) && Objects.equals(id, other.id) && Objects.equals(name, other.name);
+		return Objects.equals(cost, other.cost) && Objects.equals(product_id, other.product_id) && Objects.equals(product_name, other.product_name);
 	}
 
 	
