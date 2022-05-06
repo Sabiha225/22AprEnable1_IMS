81d5c85a4bad8cb7430a910883ab06814caac4d2
Sabiha225 <103116778+Sabiha225@users.noreply.github.com>
2022-05-06 16:33:30 +0100

    Add files via upload

---
+++ src/main/java/com/qa/ims/persistence/dao/ProductsDAO.java
@@ -0,0 +1,135 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Products;
+import com.qa.ims.utils.DBUtils;
+
+public class ProductsDAO implements Dao<Products>{
+
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	@Override
+	public Products modelFromResultSet(ResultSet resultSet) throws SQLException {
+		Long id = resultSet.getLong("product_id");
+		String name = resultSet.getString("product_name");
+		Double cost = resultSet.getDouble("cost");
+		return new Products(id, name, cost);
+	}
+	
+	/**
+	 * Reads all products from the database
+	 * 
+	 * @return A list of products 
+	 */
+	
+	@Override
+	public List<Products> readAll() {
+		try (Connection connection =DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM products");) {
+			List<Products> products = new ArrayList<>();
+			while (resultSet.next()) {
+				products.add(modelFromResultSet(resultSet));
+			}
+			return products;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+		
+	}
+	
+	public Products readLatest() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM products ORDER BY id DESC LIMIT 1");) {
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Create a product in the database 
+	 * 
+	 * @param products - takes in a product object. 
+	 */
+	
+	@Override
+	public Products create(Products product) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("INSERT INTO products(product_name, cost) VALUES (?, ?)");) {
+			statement.setString(1, product.getName());
+			statement.setDouble(2, product.getCost());
+			statement.executeUpdate();
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+	@Override
+	public Products read(Long id) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("SELECT * FROM products WHERE product_id = ?");) {
+			statement.setLong(1, id);
+			try (ResultSet resultSet = statement.executeQuery();) {
+				resultSet.next();
+				return modelFromResultSet(resultSet);
+			}
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+
+	@Override
+	public Products update(Products product) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("UPDATE products SET product_name = ?, cost = ? WHERE product_id = ?");) {
+			statement.setString(1, product.getName());
+			statement.setDouble(2, product.getCost());
+			statement.setLong(3, product.getId());
+			statement.executeUpdate();
+			return read(product.getId());
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public int delete(long id) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("DELETE FROM products WHERE product_id = ?");) {
+			statement.setLong(1, id);
+			return statement.executeUpdate();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return 0;
+	}
+
+
+}
