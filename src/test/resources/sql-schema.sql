c90be2fb18269a9908807afa978af6cc72e87a73
Sabiha225 <103116778+Sabiha225@users.noreply.github.com>
2022-05-06 16:20:44 +0100

    Update sql-schema.sql

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,8 +1,37 @@
-DROP TABLE IF EXISTS `customers`;
+drop schema ims;
 
-CREATE TABLE IF NOT EXISTS `customers` (
+CREATE SCHEMA IF NOT EXISTS `ims`;
+
+USE `ims` ;
+
+CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
+);
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+`order_id` INT(11) NOT NULL AUTO_INCREMENT,
+`customer_id` INT NOT NULL,
+`order_date` VARCHAR(10) NOT NULL,
+`total cost` DECIMAL(6,2) NOT NULL,
+PRIMARY KEY (`order_id`),
+FOREIGN KEY(`customer_id`) REFERENCES `ims`.`customers`(`customer_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`products` (
+`product_id` INT(11) NOT NULL AUTO_INCREMENT,
+`product_name` VARCHAR(300) NOT NULL,
+`cost` DECIMAL(6,2) NOT NULL,
+PRIMARY KEY(`product_id`) 
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`order_item` (
+`order_item_id` INT(11) NOT NULL AUTO_INCREMENT,
+`order_id` INT NOT NULL,
+`product_id` INT NOT NULL,
+`quantity` INT NOT NULL,
+PRIMARY KEY(`order_item_id`),
+FOREIGN KEY(`order_id`) REFERENCES `ims`.`orders`(`order_id`),
+FOREIGN KEY(`product_id`) REFERENCES `ims`.`products`(`product_id`)
 );
